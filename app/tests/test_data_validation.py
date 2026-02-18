df0e4ee0b48abd9c14a232c45069f2f42546c2b6
Doggo21345 <a12pandaa@gmail.com>
2026-02-17 20:20:57 -0600

    added fixed tests + pipelien for act

--- app/tests/test_data_validation.py
+++ app/tests/test_data_validation.py
@@ -5,9 +5,6 @@ import json
 import os
 from datetime import datetime, timedelta
 
-def calculate_day_one():
-    release_date 
-    
 
 def test_raw_data_scheam():
     sample_path = 'tests?samples/xbox_api_sample.json'
@@ -18,13 +15,4 @@ def test_raw_data_scheam():
         for col in expected_cols:
             assert col in df.columns, f"ETL logic Error: {col} missing after normalization and therefore can not be included "
 
-def test_day_one ():
-    today_str = datetime.now().strftime('%Y-%m-%d')
-    yesterday_str = (datetime.now()-timedelta(days =1)).strftime('%Y-%m-%d')
-    test_df = pd.DataFrame({
-        'product_name':['New Game', 'Old Game']
-        'Added': [today_str,. yesterday_str]
-    })
-    result_df = calculate_day_one(test_df)
-    assert result_df.loc[result_df['product_name'] == 'New Game', 'is_day_one_gp'].iloc[0] == True
-    assert result_df.loc[result_df['product_name'] == 'Old Game', 'is_day_one_gp'].iloc[0] == False
+
